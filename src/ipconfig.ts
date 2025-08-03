2058517a2e50d5823b6f350d174ca161652fcb45
John Carl Cueva <ninjadentist24@gmail.com>
2025-08-04 00:00:50 +0800

    config update

--- src/ipconfig.ts
+++ src/ipconfig.ts
@@ -1,4 +1,4 @@
-const ipconfig = 'http://localhost:8000';
-// const ipconfig = 'https://abucancode.com/testf';
+// const ipconfig = 'http://localhost:8000';
+const ipconfig = 'https://abucancode.com/testf';
 
 export default ipconfig
